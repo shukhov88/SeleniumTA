d373fd556d7b2e04d1d532abd34ef12e675587ee
Vladimir Shukhov (work) <shukhov88@gmail.com>
2017-03-02 16:13:55 +0300

    uploading changes to continue from work

--- selenium_task_1/src/test/java/Task7.java
+++ selenium_task_1/src/test/java/Task7.java
@@ -37,7 +37,7 @@ public class Task7 {
 
     @Test
     public void menuItemsTest () {
-        driver.get("http://litecart/admin/");
+        driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
