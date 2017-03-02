5d8a453978b700012b9115ee076358d81d04ed1c
Vladimir Shukhov (work) <shukhov88@gmail.com>
2017-03-02 17:35:59 +0300

    uploading version to ask mentor

--- selenium_task_1/src/test/java/Task7.java
+++ selenium_task_1/src/test/java/Task7.java
@@ -43,27 +43,33 @@ public class Task7 {
         driver.findElement(By.name("login")).click();
         wait.until(titleIs("My Store"));
 
+        //находим количество основных пунктов меню
         int itemsQuantity = driver.findElements(By.cssSelector("li#app-")).size();
 
+        //Цикл который прокликивает каждый основной пункт меню
         for (int i = 1; i < itemsQuantity + 1; i++) {
             WebElement menuItem = driver.findElement(By.cssSelector("li#app-:nth-child(" + i + ")"));
             menuItem.click();
             Assert.assertTrue(isElementPresent(By.tagName("h1")));
 
+            //находим количество вложенных пунктов меню относительно текущего основного пункта
             int subItemsQuantity = driver.findElements(By.cssSelector("[id^=doc-]")).size();
-
             //System.out.println(subItemsQuantity);
 
+            //Цикл который должен прокликать все вложенные пункты текущего основного пункта
+            //И здесь что то не так, помогите разобраться
             for (int j = 1; j < subItemsQuantity + 1; j++) {
-                WebElement subMenuItem = driver.findElement(By.cssSelector("[id^=doc-]:nth-of-type(" + i + ")"));
+                WebElement subMenuItem = driver.findElement(By.cssSelector("[id^=doc-]:nth-child(" + i + ")"));
                 subMenuItem.click();
+
+
 
                 /*List <WebElement> subMenuItemsList = driver.findElements(By.cssSelector("[id^=doc-]"));
                 System.out.println(subMenuItemsList.size());
                 subMenuItemsList.get(i).click();*/
 
                 //Assert.assertTrue(isElementPresent(By.tagName("h1")));
-            }
+              }
         }
 
 
